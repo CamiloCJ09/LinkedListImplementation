2926b81a033c33873b66be8da5b38023eddf05ca
CamiloCJ09 <camilocampazj@gmail.com>
2021-09-03 10:12:59 -0500

    Adding list implementation

---
+++ src/model/ILinkedList.java
@@ -0,0 +1,10 @@
+package model;
+
+public interface ILinkedList<T> {
+
+    public void add(T element);
+    public void delete(int index);
+    public T find(int index);
+
+
+}
---
+++ src/model/Node.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Node<E> {
+
+
+    private E item;
+    private Node<E> next;
+    private Node<E> previous;
+    private Node<E> root;
+
+    public Node(E element) {
+        this.item = element;
+        this.next = null;
+        this.previous = null;
+
+    }
+
+    public Node<E> getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node<E> root) {
+        this.root = root;
+    }
+
+    public void setItem(E item) {
+        this.item = item;
+    }
+
+    public E getItem() {
+        return item;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public Node<E> getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node<E> previous) {
+        this.previous = previous;
+    }
+}
